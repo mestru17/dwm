ed5b5c17c5f0ae0b142a892ef891065da34d29c2
mestru17 <mestru17@student.aau.dk>
2021-09-18 19:50:20 +0200

    Initial commit

---
+++ patch/include.c
@@ -0,0 +1,18 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_status2d.c"
+#include "bar_tags.c"
+
+/* Other patches */
+#include "attachx.c"
+#include "rotatestack.c"
+#include "vanitygaps.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_monocle.c"
+#include "layout_tile.c"
+
---
+++ patch/include.h
@@ -0,0 +1,17 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_status2d.h"
+#include "bar_tags.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "rotatestack.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
